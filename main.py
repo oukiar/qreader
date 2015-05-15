f89aea01dc7c965ebe855a79f4be4b5e25ce719a
oukiar <oukiar@gmail.com>
2015-05-15 10:20:47 -0500

    initialized

---
+++ main.py
@@ -0,0 +1,12 @@
+
+from kivy.uix.floatlayout import FloatLayout
+
+
+class QReader(FloatLayout):
+    pass
+
+if __name__ == '__main__':
+    
+    from kivy.base import runTouchApp
+    
+    runTouchApp(QReader() )
