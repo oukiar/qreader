101b57509b9bf856166f3ff95a3b22948ae98c2e
Oscar Alcantara <oukiar@gmail.com>
2016-10-08 10:08:59 -0500

    avance

--- main_old.py
+++ main_old.py
@@ -2,7 +2,8 @@
 Qrcode example application
 ==========================
 
-Author: Mathieu Virbel <mat@meltingrocks.com>
+Original Author: Mathieu Virbel <mat@meltingrocks.com>
+Adapted by: Oscar Alcantara <oukiar@gmail.com>
 
 Featuring:
 
@@ -21,10 +22,28 @@ from collections import namedtuple
 from kivy.lang import Builder
 from kivy.app import App
 from kivy.properties import ObjectProperty, ListProperty, BooleanProperty, \
-    NumericProperty
+    NumericProperty, StringProperty
 from kivy.uix.widget import Widget
+from kivy.uix.label import Label
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.popup import Popup
 from kivy.graphics import Color, Line
+
+from devslib.utils import ImageButton
+from devslib.utils import MessageBoxTime
+
+from kivy.uix.image import Image
+from kivy.uix.bubble import Bubble
+from kivy.uix.button import Button
+from kivy.uix.dropdown import DropDown
+
+from plyer import vibrator
+
+import os
+
+
 from jnius import autoclass, PythonJavaClass, java_method, cast
 from android.runnable import run_on_ui_thread
 
@@ -37,7 +56,7 @@ ImageScanner = autoclass('net.sourceforge.zbar.ImageScanner')
 Config = autoclass('net.sourceforge.zbar.Config')
 SurfaceView = autoclass('android.view.SurfaceView')
 LayoutParams = autoclass('android.view.ViewGroup$LayoutParams')
-Image = autoclass('net.sourceforge.zbar.Image')
+ImageZBar = autoclass('net.sourceforge.zbar.Image')
 ImageFormat = autoclass('android.graphics.ImageFormat')
 LinearLayout = autoclass('android.widget.LinearLayout')
 Symbol = autoclass('net.sourceforge.zbar.Symbol')
@@ -80,7 +99,7 @@ class SurfaceHolderCallback(PythonJavaClass):
     def surfaceDestroyed(self, surface):
         pass
 
-
+  
 class AndroidWidgetHolder(Widget):
     '''Act as a placeholder for an Android widget.
     It will automatically add / remove the android view depending if the widget
@@ -112,7 +131,8 @@ class AndroidWidgetHolder(Widget):
         activity.addContentView(view, LayoutParams(*self.size))
         view.setZOrderOnTop(True)
         view.setX(self.x)
-        view.setY(self._window.height - self.y - self.height)
+        #view.setY(self._window.height - self.y - self.height)  #center screen
+        view.setY(100)
         self._old_view = view
 
     def on_size(self, instance, size):
@@ -162,6 +182,7 @@ class AndroidCamera(Widget):
             return
 
         self._android_camera = Camera.open(self.index)
+        
 
         # create a fake surfaceview to get the previewCallback working.
         self._android_surface = SurfaceView(PythonActivity.mActivity)
@@ -181,7 +202,9 @@ class AndroidCamera(Widget):
         params = self._android_camera.getParameters()
         params.setPreviewSize(width, height)
         self._android_camera.setParameters(params)
-
+    
+        self._android_camera.setDisplayOrientation(90)
+    
         # now that we know the camera size, we'll create 2 buffers for faster
         # result (using Callback buffer approach, as described in Camera android
         # documentation)
@@ -218,6 +241,8 @@ class AndroidCamera(Widget):
             self._holder.pos = pos
 
 
+
+
 class ZbarQrcodeDetector(AnchorLayout):
     '''Widget that use the AndroidCamera and zbar to detect qrcode.
     When found, the `symbols` will be updated
@@ -228,6 +253,8 @@ class ZbarQrcodeDetector(AnchorLayout):
 
     # XXX can't work now, due to overlay.
     show_bounds = BooleanProperty(False)
+    
+    datadecoded = StringProperty()
 
     Qrcode = namedtuple('Qrcode',
             ['type', 'data', 'bounds', 'quality', 'count'])
@@ -259,7 +286,7 @@ class ZbarQrcodeDetector(AnchorLayout):
         # then start the detection on the image
         parameters = camera.getParameters()
         size = parameters.getPreviewSize()
-        barcode = Image(size.width, size.height, 'NV21')
+        barcode = ImageZBar(size.width, size.height, 'NV21')
         barcode.setData(data)
         barcode = barcode.convert('Y800')
 
@@ -268,6 +295,8 @@ class ZbarQrcodeDetector(AnchorLayout):
         if result == 0:
             self.symbols = []
             return
+
+        
 
         # we detected qrcode! extract and dispatch them
         symbols = []
@@ -281,6 +310,9 @@ class ZbarQrcodeDetector(AnchorLayout):
                 count=symbol.getCount(),
                 bounds=symbol.getBounds())
             symbols.append(qrcode)
+            
+            print symbol.getData()
+            self.datadecoded = symbol.getData()
 
         self.symbols = symbols
 
@@ -303,35 +335,207 @@ class ZbarQrcodeDetector(AnchorLayout):
                 Line(rectangle=[x, y, w, h], group='bounds')
     '''
 
+class LabelData(Popup):
+    content = ObjectProperty()
+    
+    
+class Warehouses(Object):
+    pass
+    
+class Clients(Object):
+    pass
+    
+class Detector(BoxLayout):
+    
+    detector = ObjectProperty()
+    message = ObjectProperty()
+
+class RepackForm(Popup):
+    agent = ObjectProperty()
+    consignee = ObjectProperty()
+    zone = ObjectProperty()
+    transport = ObjectProperty()
+    contentnotes = ObjectProperty()
+    generalnotes = ObjectProperty()
+    internalnotes = ObjectProperty()
+    
+class LabelReceipt(BoxLayout):
+    receipt = ObjectProperty()
+    consignee = ObjectProperty()
+    zone = ObjectProperty()
+    volume = ObjectProperty()
+    country = ObjectProperty()
+    objectId = ObjectProperty()
+
+class Repack(Popup):
+    detector = ObjectProperty()
+    message = ObjectProperty()
+    boxlabels = ObjectProperty()
+    
+    labels = []
+    
+    def on_open(self):
+        print "Opening"
+        self.detector.start()
+    
+    def on_dismiss(self):
+        print "Closing repack"
+        self.detector.stop()
+        
+    def decoded(self, val):
+        '''
+        if self.message.text != "Labels: ":
+            self.message.text += ", "
+            
+        self.message.text += val
+        '''
+        
+        if val not in self.labels:
+            self.labels.append(val)
+            
+            #obtener informacion del warehouse leido
+            wh = Warehouses.Query.get(objectId=val)
+            
+            
+            box = Bubble(size_hint_y=None, height=200)
+                    
+            labelreceipt = LabelReceipt()
+            
+            labelreceipt.receipt.text = "Receipt#: " + str(wh.Receipt)
+            labelreceipt.zone.text = "Zone: " + wh.Zone
+            labelreceipt.volume.text = "Volume: " + wh.Volume
+            labelreceipt.consignee.text = "[b]Consignee:[/b]\n" + wh.Consignee.Name
+            labelreceipt.country.text = "Country: " + wh.Country
+            labelreceipt.objectId.text = "ID: " + wh.objectId
+            
+            '''
+            labelreceipt.zone.text = wh.Zone
+            '''
+                    
+            #box.add_widget(Label(text="ID:"+val))
+            box.add_widget(labelreceipt)
+            
+            if os.path.isfile("box.png"):
+                print "BOX IMAGE FOUND"
+                                
+            self.boxlabels.add_widget( box )
+            vibrator.vibrate(.2)
+
+class Main(FloatLayout):
+    pass
+
+class QReader(FloatLayout):
+    
+    login = ObjectProperty()
+    
+    def signin(self):
+        
+        try:
+            self.user = User.login(self.login.username.text, self.login.password.text)
+        
+            self.remove_widget(self.login)
+            
+            self.main = Main()
+            self.add_widget(self.main)
+        except:
+            MessageBoxTime(msg="Bad login").open()
+        
+    
+    def decoded(self, val):
+        self.message.text = val
+        
+    def show_label(self):
+        
+        labeldata = LabelData()
+        
+        
+        labelinfo = Warehouses.Query.get(objectId=self.message.text)
+        
+        print labelinfo, labelinfo.ReceiptDate
+
+        for property, value in vars(labelinfo).iteritems():
+            print property, ": ", value
+            labeldata.content.add_widget(Label(text=str(property) ) )
+            labeldata.content.add_widget(Label(text=str(value) ) )
+
+        '''
+        labeldata.receipt.text = str(labelinfo.Receipt)
+        labeldata.status.text = labelinfo.Status
+        labeldata.receiptDate.text = str(labelinfo.ReceiptDate)
+        labeldata.agent.text = labelinfo.Agent
+        labeldata.shipper.text = labelinfo.Shipper.Name
+        labeldata.consignee.text = labelinfo.Consignee.Name
+        '''
+        
+        labeldata.open()
+        
+    def on_repack(self):
+        print "Repack !"
+        
+        self.repack = Repack()
+        self.repack.open()
+        
+    def repack_done(self):
+        self.repack.dismiss()
+        
+        #open the repack dialog information
+        self.repackform = RepackForm()
+        self.repackform.open()
+        
+    def repack_save(self):
+        self.repackform.dismiss()
+        
+        wh = Warehouses()
+        wh.Agent = self.repackform.agent.text
+        #wh.Consignee.Name = self.repackform.consignee.text
+        wh.Zone = self.repackform.zone.text
+        wh.Transport = self.repackform.transport.text
+        wh.Content = self.repackform.contentnotes.text
+        wh.GeneralNotes = self.repackform.generalnotes.text
+        wh.InternalNotes = self.repackform.internalnotes.text
+
+        wh.save()
+        
+    def do_clientcomplete(self, w):
+        print w.text
+        
+        clients = Clients.Query.all()
+
+        #for the autocomplete
+        if not hasattr(self, "down"):
+            self.down = DropDown()
+
+        self.down.clear_widgets()
+
+        for client in clients:
+            if w.text.lower() in client.Name.lower():
+                print client.Name
+                
+                but = Button(text=client.Name, size_hint_y=None, height=44, on_press=self.select_clientresult)
+                
+                self.down.add_widget(but)
+                
+        self.down.open(self.repackform.consignee)
+
+    def select_clientresult(self, w):
+        print w.text
+        
 
 if __name__ == '__main__':
 
-    qrcode_kv = '''
-BoxLayout:
-    orientation: 'vertical'
-
-    ZbarQrcodeDetector:
-        id: detector
-
-    Label:
-        text: '\\n'.join(map(repr, detector.symbols))
-        size_hint_y: None
-        height: '100dp'
-
-    BoxLayout:
-        size_hint_y: None
-        height: '48dp'
-
-        Button:
-            text: 'Scan a qrcode'
-            on_release: detector.start()
-        Button:
-            text: 'Stop detection'
-            on_release: detector.stop()
-'''
-
-    class QrcodeExample(App):
+    class QReaderApp(App):
         def build(self):
-            return Builder.load_string(qrcode_kv)
-
-    QrcodeExample().run()
+                        
+            return QReader()
+            
+        def on_stop(self):
+            pass
+        
+            
+        def on_pause(self):
+            return True
+            
+        def on_resume(self):
+            pass
+        
+    QReaderApp().run()
